6fe2a4d8f87f5ad18a26ea844cd3e2a785cadd84
dragao2elastico <dragao2elastico@offcial.github.com>
2022-05-26 14:48:32 -0300

    Atualizado!

---
+++ api.js
@@ -0,0 +1,47 @@
+/* Modulos */
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const quick = require('quick.db')
+
+/* Configurações (Não mexa) */
+const PORT = 3000 // Número alterável
+const app = new express()
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(cors())
+
+/* API */
+let db = [
+    { '1': { Nome: 'Client 1', Idade: '20' } },
+    { '2': { Nome: 'Client 2', Idade: '20' } },
+    { '3': { Nome: 'Client 3', Idade: '20' } }
+]
+
+// Buscar Dados
+app.get('/', (req, res) => {
+    return res.json(db)
+})
+
+// Inserir Dados
+app.post('/add', (req, res) => {
+    const body = req.body
+
+    if (!body) 
+        return res.status(400).end()
+
+    db.push(body)
+    return res.json(body)
+})
+
+/* Listando Servidor */
+app.listen(PORT, () => {
+    console.log(`Servidor Listado em: http://localhost:${PORT}`);
+})
+
+/* 
+
+ */
